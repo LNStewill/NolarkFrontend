3856dff8739ac1b28a37ae191596b925e60f860d
Nathan Stéwill LOUFOUA <127401929+LNStewill@users.noreply.github.com>
2023-04-07 01:59:52 +0200

    Ajout de toutes les pages du projet dans le repertoire .git

---
+++ js/draft.js
@@ -0,0 +1,48 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+
+/*const fixe = 1100;
+
+var nbAncien = window.document.querySelector("#num_ancien").value;
+var nbAncien = parseInt(window.document.querySelector("#num_ancien").value);
+window.document.querySelector("#remuneration").innerHTML = "La rémunération sera de : " + remuneration + "€";
+
+console.log("ma variable = " + maVar);//Affichera un message dand la console*/
+
+
+/*window.addEventLister("load", function() {
+    window.document.querySelector("#num_ancien").addEventListener("keyup", function() {
+        //traitements
+    }, false);
+    window.document.querySelector("#num_s20").addEventListener("keyup", function () {
+    // traitements
+    }, false);
+    window.document.querySelector("num_xspirit").addEventListener("keyup", function() {
+    //traitements
+    }, false);
+    window.document.querySelector("num_multi").addEventListener ("keyup", function() { 
+        //traitements
+    }, false);
+    window.document.querySelector ("num_km").addEventListener ("keyup", function() { 
+    //traitements
+    }, false);
+}, false);
+
+
+
+window.addEventListener("load", function () { 
+    // Declaration de l'index de parcours
+    var i;
+    // tabInputs est une collection de <input>
+    var tabInputs = window.document.querySelectorAll("input");
+    // Parcours de tabInputs en s'appuyant sur le nombre de <input> 
+    for (i=0; i < tabInputs.length ; i++) {
+        // Ajout d'un Listener sur tous les <input> sur l'événement onkeyp 
+        tabInputs[i].addEventListener("keyup", function () {
+            // traitements
+        }, false);
+    }
+}, false);*/
